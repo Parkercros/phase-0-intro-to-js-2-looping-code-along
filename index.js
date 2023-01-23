26d2f48567bb825cf7f27dd82c188a925bba81c5
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-22 18:57:01 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGift(gift) {
+  console.log(`Wrapped ${gift} and added a bow!`);
+}
+
+for (let i = 0; i < gifts.length; i++) {
+  wrapGift(gifts[i]);
+}
+
+function writeCards(names) {
+  let messages = [];
+  for (let name of names) {
+    messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
